b52576e67b1fed5d6a25ff7c3b97b8fe19b628f8
ralex2304 <ralex2304@gmail.com>
2025-03-18 17:41:08 +0300

    Added FPS macro parameter

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -12,13 +12,16 @@
 #define DEFAULT_BRIGHTNESS 6
 #define NUM_MODES          2
 
+#define FPS 30
+
 DisplayMax7219<CS_PIN, MOSI_PIN, SCK_PIN> disp;
 
-void cycle_bitmap(const int period, const byte* bmaps, const byte bmaps_num);
+void cycle_bitmap(const int frames_per_cycle, const byte* bmaps, const byte bmaps_num);
 void draw_bitmap(const byte* bmap);
 
 byte mode = 0;
 byte counter = 0;
+byte frame_counter = 0;
 
 void setup() {
     pinMode(BTN_PIN, INPUT_PULLUP);
@@ -26,13 +29,15 @@ void setup() {
     disp.begin(DEFAULT_BRIGHTNESS);
 }
 
-#define CYCLE_BITMAP(period_, bmaps_) cycle_bitmap(period_, bmaps_, sizeof(bmaps_) / disp.NUM_DIGITS)
+#define BITMAPS_AND_SIZE(bmaps_) bmaps_, (sizeof(bmaps_) / disp.NUM_DIGITS)
 
 void loop() {
-    CYCLE_BITMAP(1000, HEART_BLINK);
-    // CYCLE_BITMAP( 750, HEART_MINI);
-    // CYCLE_BITMAP( 500, HEART_HALVES);
-    // CYCLE_BITMAP( 500, HEART_MESSAGES);
+    cycle_bitmap(30, BITMAPS_AND_SIZE(HEART_BLINK));
+    // cycle_bitmap(25, BITMAPS_AND_SIZE(HEART_MINI));
+    // cycle_bitmap(15, BITMAPS_AND_SIZE(HEART_HALVES));
+    // cycle_bitmap(15, BITMAPS_AND_SIZE(HEART_MESSAGES));
+
+    delay(1000 / FPS);
 }
 
 void draw_bitmap(const byte* bmap) {
@@ -43,12 +48,13 @@ void draw_bitmap(const byte* bmap) {
     disp.update_all();
 }
 
-void cycle_bitmap(const int period, const byte* bmaps, const byte bmaps_num) {
+void cycle_bitmap(const int frames_per_cycle, const byte* bmaps, const byte bmaps_num) {
     if (counter >= bmaps_num)
         counter = 0;
 
-    draw_bitmap(bmaps + disp.NUM_DIGITS * counter++);
-
-    delay(period);
+    if (frame_counter++ >= frames_per_cycle) {
+        frame_counter = 0;
+        draw_bitmap(bmaps + disp.NUM_DIGITS * counter++);
+    }
 }
 
